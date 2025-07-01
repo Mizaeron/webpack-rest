8149ffb2aafb05ac98ed3cf18ff8d74f3b519a04
Miza <mizaeron@gmail.com>
2025-07-01 18:28:54 +0100

    Add description and nav bar

--- src/page-load.js
+++ src/page-load.js
@@ -1,10 +1,19 @@
 export function pageLoad () {
 
 const content = document.querySelector("#content");
-const headline = document.createElement("header");
+const headline = document.createElement("div");
+const description = document.createElement("div");
+const container = document.createElement("div");
 
-headline.textContent = "Amazing Burger Restaurant"
+container.classList.add("container");
 
-content.appendChild(headline);
+headline.classList.add("headline");
+headline.textContent = "Amazing Burger Restaurant";
+
+description.classList.add("description");
+description.textContent = "Premier burger restaurant in town, only 9.99$ for burger and fries"
+
+content.append(container);
+container.append(headline, description);
 
 }
