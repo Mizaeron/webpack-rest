236304cb70c5f0647be4a4b12a07ba6b20307b1b
Miza <mizaeron@gmail.com>
2025-07-07 19:49:16 +0100

    Add some menu items

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { menu } from "./menu";
 import { pageLoad } from "./page-load";
+import { image } from "./menu";
 
 //pageLoad();
 menu();
--- src/menu.js
+++ src/menu.js
@@ -2,15 +2,22 @@ export function menu () {
     const content = document.querySelector("#content");
     const headline = document.createElement("div");
     const item1 = document.createElement("div");
+    const item2 = document.createElement("div");
     const menuContainer = document.createElement("div");
+    const buy = document.createElement("button");
 
     headline.classList.add("menu");
     item1.classList.add("item1");
+    item2.classList.add("item2");
+    menuContainer.classList.add("menu-box");
 
     headline.textContent = "Menu";
     item1.textContent = "Omega Triple Cheese Burger";
+    item2.textContent = "Shrimp Mac & Cheese";
+    buy.textContent = "BUY 10.99$";
 
     content.append(menuContainer);
-    menuContainer.append(headline, item1);
+    menuContainer.append(headline, item1, buy, item2);
 }
+
 
