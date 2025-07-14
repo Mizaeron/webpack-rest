8cbc927ef15c6960e22342380041a7220d039b3a
Miza <mizaeron@gmail.com>
2025-07-14 18:51:08 +0100

    Create menu items with DOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./styles.css";
 import { menu } from "./menu";
 import { pageLoad } from "./page-load";
-import { image } from "./menu";
 
 //pageLoad();
 menu();
--- src/menu.js
+++ src/menu.js
@@ -3,21 +3,28 @@ export function menu () {
     const headline = document.createElement("div");
     const item1 = document.createElement("div");
     const item2 = document.createElement("div");
+    const item3 = document.createElement("div");
     const menuContainer = document.createElement("div");
-    const buy = document.createElement("button");
+    const buy1 = document.createElement("button");
+    const buy2 = document.createElement("button");
+    const buy3 = document.createElement("button");
 
     headline.classList.add("menu");
     item1.classList.add("item1");
     item2.classList.add("item2");
+    item3.classList.add("item3");
     menuContainer.classList.add("menu-box");
 
     headline.textContent = "Menu";
     item1.textContent = "Omega Triple Cheese Burger";
     item2.textContent = "Shrimp Mac & Cheese";
-    buy.textContent = "BUY 10.99$";
+    item3.textContent = "Housemade Grandma Burger";
+    buy1.textContent = "BUY 10.99$";
+    buy2.textContent = "BUY 12.99$";
+    buy3.textContent = "Buy 13.99$";
 
     content.append(menuContainer);
-    menuContainer.append(headline, item1, buy, item2);
+    menuContainer.append(headline, item1, buy1, item2, buy2, item3, buy3);
 }
 
 
