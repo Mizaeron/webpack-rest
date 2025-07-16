aa2d6a750cc4cfbc027dc59a9981cc626ee35f18
Miza <mizaeron@gmail.com>
2025-07-16 21:11:30 +0100

    Add tab switch logic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,34 @@ import "./styles.css";
 import { menu } from "./menu";
 import { pageLoad } from "./page-load";
 import { about } from "./about";
+import restore from "./restore.jpg";
 
-//pageLoad();
-//menu();
-about();
+pageLoad();
+
+document.querySelector("nav").addEventListener("click", (e) => {
+    if(e.target.className === "load-menu") {
+        reset();
+        menu();
+    }
+    if(e.target.className === "load-home") {
+        reset();
+        pageLoad();
+    }
+    if(e.target.className === "load-about") {
+        reset();
+        about();
+    }
+});
+
+function reset () {
+    const content = document.getElementById("content");
+
+   content.innerHTML = "";
+   
+   const image = document.createElement("img");
+   image.src = restore;
+
+   content.append(image);
+
+}
+
